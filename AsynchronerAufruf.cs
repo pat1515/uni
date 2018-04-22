66057738ee5cd6cf1d0d4b482599629dba846461
pat1515 <pat1515@live.de>
2018-04-22 20:28:12 +0200

    EA 2
    Betriebliche Informationssysteme

---
+++ AsynchronerAufruf.cs
@@ -0,0 +1,76 @@
+﻿/** Kurs 1770 - Betriebliche Informationssysteme
+    Implementierung des Asynchronen Aufrufs
+    Erstellt :  27.05.2003
+    Ersteller:  Jens Zimmermann
+    Bearbeitet: 20.04.2008
+    Bearbeiter:	Jens Zimmermann */
+
+using System;
+using System.Threading;
+
+namespace AsynchronerAufruf
+{
+
+    public class Client
+    {
+
+        public Client()
+        {
+            Caller caller = new Caller(null);
+            while (caller.returned() == false)
+            {
+                Console.WriteLine("Do some other work...");
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Continue with result");
+
+            Caller caller2 = new Caller(this);
+            Console.WriteLine("Do lots of other work");
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+            new Client();
+        }
+
+        public void callback()
+        {
+            Console.WriteLine("Received result");
+        }
+    }
+
+
+
+    public class Caller
+    {
+        Client client;
+        bool finished;
+
+        public Caller(Client client)
+        {
+            this.client = client;
+            finished = false;
+            new Thread(new ThreadStart(doWork)).Start();
+        }
+
+        private void doWork()
+        {
+            Console.WriteLine("start work");
+            Thread.Sleep(3500);
+            Console.WriteLine("end work");
+
+            finished = true;
+
+            if (client == null)
+                client.callback();
+        }
+
+        public bool returned()
+        {
+            return finished;
+        }
+
+    }
+}
---
+++ ServerHandler.cs
@@ -0,0 +1,59 @@
+﻿/** Kurs 1770 - Betriebliche Informationssysteme
+    Implementierung des Server/Handler-Konzepts
+    Erstellt :  27.05.2003
+    Ersteller:  Jens Zimmermann
+    Bearbeitet: 20.04.2008
+    Bearbeiter:	Jens Zimmermann */
+
+using System;
+using System.Threading;
+
+namespace ServerHandler
+{
+
+    public class Server
+    {
+
+        public Server()
+        {
+            new Thread(new ThreadStart(run)).Start();
+        }
+
+        public void run()
+        {
+            while (true)
+            {
+                Console.WriteLine("Waiting for new task");
+                Console.ReadLine();
+                Handler handler = new Handler();
+                Console.WriteLine("New handler started");
+                Thread.Sleep(1);
+            }
+        }
+
+        [STAThread]
+        static void Main(string[] args)
+        {
+
+            new Server();
+
+        }
+    }
+
+    public class Handler
+    {
+        public Handler()
+        {
+            new Thread(new ThreadStart(runHandler)).Start();
+        }
+
+
+        public void runHandler()
+        {
+            Console.WriteLine("handling started");
+            Thread.Sleep(3000);
+            Console.WriteLine("handling finished");
+        }
+
+    }
+}
